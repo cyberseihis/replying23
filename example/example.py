a57988f5537097884e09ca372451da1958c68605
irene_rouh <st1059654@ceid.upatras.gr>
2023-03-09 13:44:48 +0200

    Change README

---
+++ example/example.py
@@ -0,0 +1,10 @@
+import os      # for interacting with the operating system
+import sys     # for interacting with the Python interpreter
+import math    # for mathematical operations
+import random  # for generating random numbers
+import time    # for working with dates and times
+
+from math import sqrt
+
+result = sqrt(25)
+print(result)
